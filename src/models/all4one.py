6369b99128b4b28411299199c844d3621efe2dfc
llzmgjzhy <guangzewei@foxmail.com>
2025-04-14 15:41:25 +0800

    v1.5 fea: add res connct; fix image flip issue; mask_scatter add total image token.

--- src/models/all4one.py
+++ src/models/all4one.py
@@ -17,6 +17,32 @@ import matplotlib.pyplot as plt
 import os
 
 
+class Mlp(nn.Module):
+    def __init__(
+        self,
+        in_features,
+        hidden_features=None,
+        out_features=None,
+        act_layer=nn.GELU,
+        drop=0.0,
+    ):
+        super().__init__()
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+        self.fc1 = nn.Linear(in_features, hidden_features)
+        self.act = act_layer()
+        self.fc2 = nn.Linear(hidden_features, out_features)
+        self.drop = nn.Dropout(drop)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.act(x)
+        x = self.drop(x)
+        x = self.fc2(x)
+        x = self.drop(x)
+        return x
+
+
 class FlattenHead(nn.Module):
     def __init__(self, nf, seq_window, head_dropout=0):
         super().__init__()
@@ -183,19 +209,34 @@ class ALL4ONEFAST(nn.Module):
         # visual module
         self.visual = self.model.visual
         self.image_processor = Qwen2VLImageProcessorFast()
+        self.merge_length = self.image_processor.merge_size**2
 
         # processor
-        self.tokernizer = Qwen2_5_VLProcessor.from_pretrained(
+        self.tokenizer = Qwen2_5_VLProcessor.from_pretrained(
             "Qwen/Qwen2.5-VL-7B-Instruct",
             attn_implementation="flash_attention_2",
             torch_dtype=torch.bfloat16,
         ).tokenizer
+
+        self.image_token = (
+            "<|image_pad|>"
+            if not hasattr(self.tokenizer, "image_token")
+            else self.tokenizer.image_token
+        )
 
         # ts2vec embedding align llm input dims
         self.ts2vec_embedding = PatchEmbedding(
             config.llm_dim, config.patch_size, config.stride, config.dropout
         ).to(dtype=torch.bfloat16, device=device)
         self.patch_nums = int((config.seq_len - self.patch_size) / self.stride + 2)
+
+        # residual embedding, token embed on x_enc, and llm output will be added, speed up training
+        self.x_enc_residual_embed = Mlp(
+            in_features=self.seq_len,
+            hidden_features=config.d_model,
+            out_features=self.pred_len,
+            drop=config.dropout,
+        ).to(dtype=torch.bfloat16, device=device)
 
         # outprojection
         if config.task == "forecast":
@@ -218,6 +259,10 @@ class ALL4ONEFAST(nn.Module):
     def forward(self, x_enc, x_mask, y, y_mask, stage="train", batch_idx=0):
         B, T, N = x_enc.shape
         x_enc = self.normalize_layers(x_enc, "norm")
+
+        x_enc_residual = self.x_enc_residual_embed(x_enc.permute(0, 2, 1)).transpose(
+            1, 2
+        )  # [B, pred_len, N]
 
         # statistics prompt
         x_enc = x_enc.permute(0, 2, 1).contiguous().reshape(B * N, T, 1)
@@ -247,8 +292,65 @@ class ALL4ONEFAST(nn.Module):
 
         x_enc = x_enc.reshape(B, N, T).permute(0, 2, 1).contiguous()
 
+        # check cache
+        cache_file = os.path.join(self.cache_dir, f"{stage}_{batch_idx}.pt")
+
+        if os.path.exists(cache_file):
+            images_embeds = torch.load(
+                cache_file, map_location=x_enc.device, weights_only=False
+            )["data"]
+            image_grid_thw = torch.load(
+                cache_file, map_location=x_enc.device, weights_only=False
+            )["image_grid_thw"]
+            image_token_nums = torch.load(
+                cache_file, map_location=x_enc.device, weights_only=False
+            )["image_token_nums"]
+        else:
+            # create image tensor
+            x_image = tensor_line_plots(x_enc, flip=True)  # [B, 3, H, W]
+            image_inputs = self.image_processor(
+                images=x_image, videos=None, do_rescale=False
+            ).to(device=x_enc.device, dtype=x_enc.dtype)
+            images_embeds = self.visual(
+                image_inputs["pixel_values"], grid_thw=image_inputs["image_grid_thw"]
+            )
+            image_grid_thw = image_inputs["image_grid_thw"]
+            # recovery batch dim depend on image_grid_thw
+            image_token_nums = [
+                thw.prod().item() // self.merge_length for thw in image_grid_thw
+            ]
+            split_embeds = torch.split(images_embeds, image_token_nums, dim=0)
+            images_embeds = torch.nn.utils.rnn.pad_sequence(
+                split_embeds, batch_first=True
+            )
+
+            # images_embeds = images_embeds.reshape(B, -1, images_embeds.shape[-1])
+
+            # save cache
+            torch.save(
+                {
+                    "data": images_embeds.cpu(),
+                    "image_grid_thw": image_inputs["image_grid_thw"].cpu(),
+                    "image_token_nums": image_token_nums,
+                    "dtype": "bfloat16",
+                },
+                cache_file,
+                _use_new_zipfile_serialization=True,
+            )
+
+        # expand image token depend on the actual image_grid_thw
+        if image_grid_thw is not None:
+            for i in range(len(prompt)):
+                while self.image_token in prompt[i]:
+                    prompt[i] = prompt[i].replace(
+                        self.image_token,
+                        "<|placeholder|>" * (image_token_nums[i]),
+                        1,
+                    )
+                prompt[i] = prompt[i].replace("<|placeholder|>", self.image_token)
+
         prompt = (
-            self.tokernizer(
+            self.tokenizer(
                 prompt,
                 return_tensors="pt",
                 padding=True,
@@ -261,34 +363,6 @@ class ALL4ONEFAST(nn.Module):
         prompt_embeddings = self.llm_model.get_input_embeddings()(
             prompt.to(x_enc.device)
         )  # (batch, prompt_token, dim)
-
-        # check cache
-        cache_file = os.path.join(self.cache_dir, f"{stage}_{batch_idx}.pt")
-
-        if os.path.exists(cache_file):
-            images_embeds = torch.load(
-                cache_file, map_location=x_enc.device, weights_only=False
-            )["data"]
-        else:
-            # create image tensor
-            x_image = tensor_line_plots(x_enc)  # [B, 3, H, W]
-            image_inputs = self.image_processor(
-                images=x_image, videos=None, do_rescale=False
-            ).to(device=x_enc.device, dtype=x_enc.dtype)
-            images_embeds = self.visual(
-                image_inputs["pixel_values"], grid_thw=image_inputs["image_grid_thw"]
-            )
-            images_embeds = images_embeds.reshape(B, -1, images_embeds.shape[-1])
-
-            # save cache
-            torch.save(
-                {
-                    "data": images_embeds.cpu(),
-                    "dtype": "bfloat16",
-                },
-                cache_file,
-                _use_new_zipfile_serialization=True,
-            )
 
         # x_enc embedding
         x_enc_embed, n_vars = self.ts2vec_embedding(
@@ -309,11 +383,9 @@ class ALL4ONEFAST(nn.Module):
         im_end_embed = self.llm_model.get_input_embeddings()(im_end_token_id_tensor)
         im_end_embed = im_end_embed.unsqueeze(0).expand(B, 1, -1).contiguous()
 
-        prompt_embeddings = torch.cat(
+        llm_enc_out = torch.cat(
             [prompt_embeddings, x_enc_embed, im_end_embed], dim=1
-        )
-
-        llm_enc_out = prompt_embeddings  # [B, token_num , llm_dim]
+        )  # [B, token_num , llm_dim]
         dec_out = self.llm_model(inputs_embeds=llm_enc_out).last_hidden_state
         dec_out = dec_out[:, -(self.patch_nums + 1) : -1, : self.seq_len]
 
@@ -321,6 +393,8 @@ class ALL4ONEFAST(nn.Module):
         dec_out = self.output_projection(dec_out).unsqueeze(
             -1
         )  # [B, pred_len, output_dim]
+        # residual connection
+        dec_out = dec_out + x_enc_residual
         dec_out = self.normalize_layers(dec_out, "denorm")
 
         return dec_out
